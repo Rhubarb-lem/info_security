53ca682bac1aab9ae885cc4d551575a7ae66700f
Fothles <fforcaken@gmail.com>
2024-10-14 12:04:33 +0700

    Fix Elgamal Encrypt|Decrypt declaration

--- libs/myciphers.h
+++ libs/myciphers.h
@@ -1,7 +1,7 @@
 #pragma once
-#include"mycrypto.h"
+#include "mycrypto.h"
 
 void ElgamalGenPG(long long int *P, long long int *g);
 void ElgamalGenKeys(long long int *C, long long int *D, long long int P, long long int g);
-char ElgamalEncryptMessage(long long int D, long long int P, long long int g, char m, long long int *r);
-char  ElgamalDecryptMessage(long long int C, long long int P,  long long int e, long long int r);
+long long int ElgamalEncryptMessage(long long int D, long long int P, long long int g, long long int m, long long int *r);
+long long int ElgamalDecryptMessage(long long int C, long long int P, long long int e, long long int r);
