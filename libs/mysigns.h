fb13d46cc59e3de799f355b8b29c29e44807e204
Fothles <fforcaken@gmail.com>
2024-11-04 17:23:57 +0700

    Something about GOST

--- libs/mysigns.h
+++ libs/mysigns.h
@@ -5,6 +5,11 @@
 #include <openssl/bn.h>
 #include "mycrypto.h"
 #include "myciphers.h"
+#include <openssl/evp.h>
+#include <openssl/objects.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/engine.h>
 
 void compute_md5(const char *str, unsigned char *result);
 void RSASign(const char *message, long long N, long long c, BIGNUM *signature);
@@ -17,3 +22,9 @@ int ElgamalSign(const char *message, long long X_val, long long P_val, long long
 int ElgamalcheckSign(const char *message, long long Y_val, long long P_val, long long G_val, long long r, long long s);
 int ElgamalSignFile(const char *file_path, long long X_val, long long P_val, long long G_val, const char *signature_file);
 int ElgamalCheckFileSignature(const char *file_path, long long Y_val, long long P_val, long long G_val, const char *signature_file) ;
+
+int GOSTSign(const char *message, BIGNUM *private_key, BIGNUM *public_key, BIGNUM *signature_r, BIGNUM *signature_s);
+int GOSTVerify(const char *message, BIGNUM *public_key, BIGNUM *signature_r, BIGNUM *signature_s);
+int GOSTSignFile(const char *file_path, BIGNUM *private_key, BIGNUM *public_key, const char *signature_path);
+int GOSTVerifyFile(const char *file_path, BIGNUM *public_key, const char *signature_path);
+int GOSTGenerateKeys(BIGNUM *private_key, BIGNUM *public_key);
